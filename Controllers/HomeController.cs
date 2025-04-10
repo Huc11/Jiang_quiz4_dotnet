2e1f7d9c11bab6bbc768df1af6f1e5b9627d9bff
Jianghai107 <jhu107@my.centennialcollege.ca>
2025-04-10 13:51:17 -0400

    Add database access message

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,26 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using JiangQuiz4App.Models;
 
 namespace JiangQuiz4App.Controllers;
-
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
-
     public IActionResult Index()
     {
+        ViewBag.Message = "Database access has already been done on April 1, 2025 by Jiang Hai";
         return View();
     }
-
     public IActionResult Privacy()
     {
         return View();
     }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
